7fd66cf80d049bef9160382d14eeeb44f8282b95
Nicolas Rosal <nicolasrosal98@gmail.com>
2022-03-22 16:13:18 +0000

    Basic Animation in React Three Fiber

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import { greet } from "./utils/greet";
+import { Html, OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import RigCharacter from "./utils/RigCharacter";
 
-function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+export default function App(): JSX.Element {  
+  
+  return (
+    <Canvas
+    >
+      <Suspense fallback={<Html>Loading...</Html>}>
+        <RigCharacter rotation={[0,Math.PI, 0]} position={[0,0,3]}/>
+      </Suspense>
+      
+      <directionalLight position={[-10,0,0]} />
+      <ambientLight intensity={0.5}/>
+      {/* <gridHelper args={[10, 10, `white`, `gray`]} /> */}
+      <axesHelper scale={[5,5,5]} position={[0,0,0]}/>
+      <OrbitControls />
+    </Canvas>
+  );
 }
-
-export default App;
